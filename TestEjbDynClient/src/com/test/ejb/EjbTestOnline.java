a58f31585da26b00a7205f6f5f12d6d20ae9abaa
gourab <gourabmanna29@gmail.com>
2017-11-22 22:57:58 +0530

    change in java class from bash

--- TestEjbDynClient/src/com/test/ejb/EjbTestOnline.java
+++ TestEjbDynClient/src/com/test/ejb/EjbTestOnline.java
@@ -15,9 +15,6 @@ import com.facade.TestingFacade;
 @SessionScoped
 public class EjbTestOnline implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String name = "";
 	private String greet;
