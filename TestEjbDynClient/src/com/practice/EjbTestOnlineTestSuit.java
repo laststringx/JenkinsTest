4bcff38cb78f5cceb01f122ad37b6340b4a8307f
gourab <gourabmanna29@gmail.com>
2017-11-25 22:02:15 +0530

    test suit added

---
+++ TestEjbDynClient/src/com/practice/EjbTestOnlineTestSuit.java
@@ -0,0 +1,13 @@
+package com.practice;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   EjbTestOnlineTest.class,
+})
+
+public class EjbTestOnlineTestSuit {
+
+}
